57d061e483712f59a4d803b38cc6c2283e498ac4
MillerFelix <millerredigolo@gmail.com>
2025-02-06 12:31:35 -0300

    Goal responsiveness adjustment

--- src/components/goals/GoalCard.jsx
+++ src/components/goals/GoalCard.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { expenseCategories, incomeCategories } from "../category/CategoryList";
 import ProgressBar from "./ProgressBar";
 import { FaTrashAlt } from "react-icons/fa";
@@ -13,12 +12,11 @@ function GoalCard({ goal, onDelete }) {
 
   function getCardStyle(type) {
     const baseStyle =
-      "p-6 rounded-xl shadow-lg transition-all duration-300 ease-in-out text-white relative overflow-hidden";
+      "p-4 md:p-6 rounded-xl shadow-lg transition-all duration-300 ease-in-out text-white relative overflow-hidden";
     const typeColors = {
       expense: "bg-gradient-to-br from-red-600 via-orange-600 to-red-800",
       income: "bg-gradient-to-br from-emerald-800 via-lime-500 to-green-800",
     };
-
     return `${baseStyle} ${typeColors[type] || "bg-gray-500"}`;
   }
 
@@ -32,31 +30,32 @@ function GoalCard({ goal, onDelete }) {
   return (
     <div className={`${getCardStyle(categoryDetails?.type)}`}>
       <div className="absolute inset-0 opacity-10 bg-white rounded-xl blur-xl"></div>
-
       <div className="relative z-10">
-        <div className="flex items-center mb-4">
-          {categoryDetails?.icon && (
-            <div className="w-12 h-12 mr-4 text-2xl flex items-center justify-center bg-yellow-400 bg-opacity-60 rounded-full p-2 shadow-md">
-              {categoryDetails.icon}
-            </div>
-          )}
-          <h3 className="text-lg font-semibold tracking-wide uppercase">
-            {goal.category}
-          </h3>
+        <div className="flex items-center justify-between mb-4">
+          <div className="flex items-center gap-2 md:gap-4 w-full">
+            {categoryDetails?.icon && (
+              <div className="w-10 h-10 md:w-12 md:h-12 text-xl flex items-center justify-center bg-yellow-400 bg-opacity-60 rounded-full p-2 shadow-md">
+                {categoryDetails.icon}
+              </div>
+            )}
+            <h3 className="text-sm md:text-lg font-semibold tracking-wide uppercase truncate w-full">
+              {goal.category}
+            </h3>
+          </div>
           <button
             onClick={() => onDelete(goal.id)}
-            className="p-2 absolute top-2 right-2 text-yellow-300 hover:text-yellow-400 transition-transform duration-200 active:scale-95 hover:scale-105"
+            className="p-1 md:p-2 text-yellow-300 hover:text-yellow-400 transition-transform duration-200 active:scale-95 hover:scale-105"
           >
-            <FaTrashAlt />
+            <FaTrashAlt className="text-sm md:text-base" />
           </button>
         </div>
-        <p className="mb-2 text-sm">
+        <p className="text-xs md:text-sm mb-1">
           <strong>
             {categoryDetails?.type === "expense" ? "Limite" : "Meta"}:
           </strong>{" "}
           R${goal.goalValue.toFixed(2)}
         </p>
-        <p className="mb-2 text-sm">
+        <p className="text-xs md:text-sm mb-1">
           <strong>Atual:</strong> R${goal.currentValue.toFixed(2)}
         </p>
         <ProgressBar
@@ -64,11 +63,11 @@ function GoalCard({ goal, onDelete }) {
           currentValue={goal.currentValue}
           goalValue={goal.goalValue}
         />
-        <p className="text-sm">
+        <p className="text-xs md:text-sm">
           <strong>Período:</strong> {formatDate(goal.startDate)} -{" "}
           {formatDate(goal.endDate)}
         </p>
-        <p className="text-sm mt-2">
+        <p className="text-xs md:text-sm mt-2">
           <strong>
             {categoryDetails?.type === "expense" ? "Utilizado" : "Progresso"}:
           </strong>{" "}
--- src/components/goals/ProgressBar.jsx
+++ src/components/goals/ProgressBar.jsx
@@ -1,11 +1,9 @@
-import React from "react";
-
 function ProgressBar({ type, currentValue, goalValue }) {
   const progressPercentage = Math.min((currentValue / goalValue) * 100, 100);
   const isComplete = progressPercentage === 100;
 
   return (
-    <div className="relative h-8 rounded-lg overflow-hidden mb-3">
+    <div className="relative h-8 rounded-lg overflow-hidden mb-3 flex items-center justify-center">
       <div className="absolute top-0 left-0 h-full w-full bg-gray-300" />
       <div
         className={`absolute top-0 left-0 h-full transition-all duration-300 ${
@@ -13,12 +11,10 @@ function ProgressBar({ type, currentValue, goalValue }) {
             ? "bg-gradient-to-r from-red-600 via-red-800 to-red-900"
             : "bg-gradient-to-r from-emerald-300 via-emerald-500 to-green-700"
         }`}
-        style={{
-          width: `${progressPercentage}%`,
-        }}
+        style={{ width: `${progressPercentage}%` }}
       />
       <span
-        className={`absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 font-semibold text-lg ${
+        className={`relative font-semibold text-xs sm:text-sm md:text-base lg:text-lg whitespace-nowrap px-2 ${
           isComplete
             ? "text-transparent bg-clip-text bg-gradient-to-r from-yellow-300 via-white to-yellow-400 animate-pulse"
             : "text-white"
