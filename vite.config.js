95c1b939b74db3bacf212c500038685521c459df
MillerFelix <millerredigolo@gmail.com>
2026-02-26 13:57:35 -0300

    Setting up PWA and adjusting the start screen

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,57 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate", // Atualiza o app no celular do usuário automaticamente quando você lança versão nova
+      includeAssets: [
+        "favicon.ico",
+        "apple-touch-icon.png",
+        "pwa-192x192.png",
+        "pwa-512x512.png",
+      ],
+      manifest: {
+        name: "Cash$Flow",
+        short_name: "CashFlow",
+        description: "Seu assistente financeiro inteligente",
+        theme_color: "#22c55e", // Cor da barra de status no topo do celular (verde do seu app)
+        background_color: "#f3f4f6", // Cor de fundo na tela de carregamento (gray-100)
+        display: "standalone", // ISSO AQUI é o que esconde a barra do navegador e deixa com cara de App nativo!
+        start_url: "/",
+        orientation: "portrait",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable", // Ajuda o Android a moldar o ícone perfeitamente
+          },
+        ],
+      },
+    }),
+  ],
   build: {
-    // Aqui dizemos para o Vite como ele deve "empacotar" o projeto
     rollupOptions: {
       output: {
         manualChunks: {
-          // 1. Pega tudo que é do React e coloca num pacote chamado 'vendor'
           vendor: ["react", "react-dom", "react-router-dom"],
-
-          // 2. Pega tudo que é do Firebase e coloca num pacote separado chamado 'firebase'
           firebase: ["firebase/app", "firebase/auth", "firebase/firestore"],
         },
       },
     },
-    // Opcional: Avisa o Vite para não reclamar se um arquivo ficar um pouquinho grande
     chunkSizeWarningLimit: 800,
   },
 });
